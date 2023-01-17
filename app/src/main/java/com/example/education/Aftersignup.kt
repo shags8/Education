8e11551fb7b8ad215e18eba9a0278664019b76c1
Shagun <shagunchahar8@gmail.com>
2023-01-17 17:25:08 +0530

    commit

--- app/src/main/java/com/example/education/Aftersignup.kt
+++ app/src/main/java/com/example/education/Aftersignup.kt
@@ -1,5 +1,7 @@
 package com.example.education
 
+import android.content.Intent
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,29 +9,43 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.example.education.databinding.ActivityAftersignupBinding
+import com.google.android.material.navigationrail.NavigationRailMenuView
 
 class Aftersignup : AppCompatActivity() {
 
     private lateinit var binding : ActivityAftersignupBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAftersignupBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        var name = binding.Name.text.toString()
-        var Username = binding.UserName.text.toString()
-        var PhNo = binding.PhNo.text.toString()
-        var Otp = binding.Otp.text.toString()
-        var Age = binding.Age.text.toString()
+        var name = binding.Name.text
+        var Username = binding.UserName.text
+        var PhNo = binding.PhNo.text
+        var Otp = binding.Otp.text
+        var Age = binding.Age.text
         setupSpinner()
+        binding.button.isEnabled = false
+        binding.button.setOnClickListener {
+            checkdetails()
+        }
     }
 
     private fun setupSpinner() {
-        val personNames = arrayOf("Rahul", "Jack", "Rajeev", "Aryan", "Rashmi", "Jaspreet", "Akbar")
+
+        var name = binding.Name.text
+        var Username = binding.UserName.text
+        var PhNo = binding.PhNo.text
+        var Otp = binding.Otp.text
+        var Age = binding.Age.text
+        val personNames = arrayOf("Choose Education","Pre-Primary", "Primary", "Vocational Courses", "Mentorship")
         val spinner = binding.spinner
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, personNames)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = arrayAdapter
+
 
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
@@ -38,13 +54,47 @@ class Aftersignup : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
+                var selecteditem = parent.getItemAtPosition(position)
+                if (name.isNotBlank()&&Username.isNotBlank()&&PhNo.isNotBlank()&&Otp.isNotBlank()&&Age.isNotBlank()){
+                    binding.button.isEnabled = parent.getItemAtPosition(0) != selecteditem
+                }
+                else{
+                    Toast.makeText(this@Aftersignup,"Fill All Out",Toast.LENGTH_LONG).show()
+                }
 
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
-                Toast.makeText(this@Aftersignup,"need to select something",Toast.LENGTH_SHORT)
+                Toast.makeText(this@Aftersignup,"need to select something",Toast.LENGTH_SHORT).show()
             }
         }
 
+    }
+    fun checkdetails()
+    {
+        var name = binding.Name.text
+        var Username = binding.UserName.text
+        var PhNo = binding.PhNo.text
+        var Otp = binding.Otp.text
+        var Age = binding.Age.text
+            if (PhNo.toString().length == 10)
+            {
+                if (Integer.parseInt(Age.toString()) <= 100){
+                    if (Otp.toString().length==6){
+                        val intent = Intent(this@Aftersignup,MainActivity::class.java)
+                        startActivity(intent)
+
+                    }
+                    else{
+                        Toast.makeText(this@Aftersignup,"Enter Correct Otp",Toast.LENGTH_SHORT).show()
+                    }
+                }
+                else{
+                    Toast.makeText(this@Aftersignup,"Enter Correct Age",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(this@Aftersignup,"Enter Correct Number",Toast.LENGTH_SHORT).show()
+            }
     }
 }
