9b0e16af33a35e3e6ed5a22ebbf5b4d30bb2132b
Shagun <shagunchahar8@gmail.com>
2023-01-20 23:41:16 +0530

    Google login

---
+++ app/src/main/java/com/example/education/Email.kt
@@ -0,0 +1,11 @@
+package com.example.education
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Email : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email)
+    }
+}
--- app/src/main/java/com/example/education/MainActivity.kt
+++ app/src/main/java/com/example/education/MainActivity.kt
@@ -1,11 +1,94 @@
 package com.example.education
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.education.databinding.ActivityAftersignupBinding
+import com.example.education.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var googlesignin  : GoogleSignInClient
+    private lateinit var binding  : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth= Firebase.auth
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googlesignin = GoogleSignIn.getClient(this@MainActivity,gso)
+
+        binding.google.setOnClickListener {
+            signInGoogle()
+        }
+        binding.email.setOnClickListener {
+            val intent = Intent(this,Email::class.java)
+            startActivity(intent)
+        }
+        binding.phone.setOnClickListener {
+            val intent = Intent(this,Phone_Number::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+    private fun signInGoogle(){
+        val signInIntent = googlesignin.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if (result.resultCode == Activity.RESULT_OK){
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }
+        }else{
+            Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful){
+                val intent : Intent = Intent(this , Aftersignup::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 }
