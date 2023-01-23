9a94f3dc0b6ab3c4e5c57565dae6c41012da7f7c
Shagun <shagunchahar8@gmail.com>
2023-01-23 13:34:53 +0530

    Homeapage

--- app/src/main/java/com/example/education/HomePage.kt
+++ app/src/main/java/com/example/education/HomePage.kt
@@ -2,12 +2,16 @@ package com.example.education
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.education.databinding.ActivityAftersignupBinding
 import com.example.education.databinding.ActivityHomePageBinding
 
 class HomePage : AppCompatActivity() {
 
     private lateinit var binding : ActivityHomePageBinding
+    lateinit var toggle : ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,25 @@ class HomePage : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        toggle = ActionBarDrawerToggle(this , binding.Homepage , R.string.open , R.string.close)
+        binding.Homepage.addDrawerListener(toggle)
+        toggle.syncState()
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.side.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> Toast.makeText(this,"ok",Toast.LENGTH_SHORT).show()
+            }
+            true
+
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
